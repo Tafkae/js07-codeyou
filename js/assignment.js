45438c8250c544bafb1fa8fb43023100df4d494f
Lisa H. <neuroticNanobot@outlook.com>
2025-01-16 23:59:05 -0500

    completed

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,26 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+
+function returnTheRemainder(num1, num2) {
+  return num1 % num2;
+}
+
+function checkIfEven(num) {
+  return (num % 2 === 0);
+}
+
+function getTheFourthElement(arr) {
+  return arr[3];
+}
+
+function render() {
+  remainderElement.innerText = returnTheRemainder(5,4);
+  checkIfEvenElement.innerText = checkIfEven(47);
+  lostNumbersElement.innerText = getTheFourthElement(lostNumbers);
+}
+
+submissionBtn.addEventListener("click", function() {
+  render();
+})
